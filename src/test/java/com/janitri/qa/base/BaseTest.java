5c8ef9e631b4e2530575d8b681f44af3019e0270
Naredla Sainath Reddy <sainathreddynaredla0@gmail.com>
2025-08-03 12:45:40 +0530

    Add manual test cases and handle browser notifications

--- src/test/java/com/janitri/qa/base/BaseTest.java
+++ src/test/java/com/janitri/qa/base/BaseTest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
 
@@ -13,16 +16,26 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
+        // Create a map to disable notification popups
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+
+        // Create a ChromeOptions object and set the preference
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
+
         // Sets up the Chrome driver automatically
         WebDriverManager.chromedriver().setup();
-        // Initializes the WebDriver
-        driver = new ChromeDriver();
+
+        // Initializes the WebDriver with the new options
+        driver = new ChromeDriver(options);
+
         // Maximizes the browser window
         driver.manage().window().maximize();
         // Sets an implicit wait time
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         // Navigates to the test page URL
-        driver.get("https://dev-dash.janitri.in/"); // [cite: 6]
+        driver.get("https://dev-dash.janitri.in/");
     }
 
     @AfterMethod
